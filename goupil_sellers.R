793e916740f32bddea205bc29a880e70c38b2d86
jacquelynhelene <jacquelynhelene@gmail.com>
2018-08-20 15:12:38 -0700

    Summarized by STAR no

--- goupil_sellers.R
+++ goupil_sellers.R
@@ -1,11 +1,14 @@
 library(tidyverse)
-library(dplyr)
 
 #Load Goupil Sellers CSV
 goupil_sellers <- read.csv("goupil_sellers.csv")
 
 goupil_sellers_filtered <- goupil_sellers %>%
-  select(STAR, Sell1, prefLabel_1)
-  group_by(goupil_sellers_filtered, Sell1, add = TRUE)
-  mutate(goupil_sellers_filtered, STAR)
-  
+  select(STAR, Sell1, prefLabel_1) %>%
+  group_by(Sell1) %>%
+  summarize(STAR_no = paste(STAR, collapse=", "))
+
+goupil_sellers_pref <- goupil_sellers %>%
+  select(STAR, Sell1, prefLabel_1, prefLabel_2, prefLabel_3, prefLabel_4) %>%
+  left_join(goupil_sellers_filtered, by = c("STAR_no"))
+            
