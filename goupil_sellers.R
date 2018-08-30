8191a7d6e149aea935e7700b8de0cef30b3053a8
jacquelynhelene <jacquelynhelene@gmail.com>
2018-08-30 11:56:21 -0700

    final commit; this code is now superseded by goupil_sellers_recon.R.

--- goupil_sellers.R
+++ goupil_sellers.R
@@ -4,11 +4,18 @@ library(tidyverse)
 goupil_sellers <- read.csv("goupil_sellers.csv")
 
 goupil_sellers_filtered <- goupil_sellers %>%
-  select(STAR, Sell1, prefLabel_1) %>%
+  select(STAR, Sell1, prefLabel_1, prefLabel_2, prefLabel_3) %>%
   group_by(Sell1) %>%
-  summarize(STAR_no = paste(STAR, collapse=", "))
+  summarize(STAR_no = paste(STAR, collapse=", "),
+            prefLabel_1 = first(prefLabel_1),
+            prefLabel_2 = first(prefLabel_2),
+            prefLabel_3 = first(prefLabel_3)) %>%
+  gather("prefLabel", "name", 3:5)
+
+
+
 
 goupil_sellers_pref <- goupil_sellers %>%
   select(STAR, Sell1, prefLabel_1, prefLabel_2, prefLabel_3, prefLabel_4) %>%
-  left_join(goupil_sellers_filtered, by = c("STAR_no"))
+  left_join(goupil_sellers, by = c("STAR"))
             
