c892fc27a02d2e7e27a71de86deea17fc548568a
sandra van ginhoven <sandra.van.ginhoven@gmail.com>
2018-09-04 19:23:49 -0700

    updated code to work with goupil csv exported from STAR

--- goupil_unique_objects_uid.R
+++ goupil_unique_objects_uid.R
@@ -1,18 +1,54 @@
 # Goupil Stock Numbers and Unique Object IDs
 
+library(tidyverse)
+library(ggplot2)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(broom)
+library(lubridate)
+library(stringr)
+library(readr)
+library(knitr)
+library(tidytext)
+library(stringr)
+library(ggraph)
+library(ggrepel)
+library(igraph)
+
+
+# Goupil concordance sheet:
+# TO DO: use "persistent_uid" = "GOUPIL-XXX" numbers (star record numbers)
+# TO DO: Note that there are some stock numbers not inlcuded -- check what is left behind in STAR export CSV
+# TO DO: also check again those "grouped" objects that the sheet is uniting -- put them under goupil_untrustworthy_numbers?
+raw_goupil_stocknumber_concordance <- read_csv("/Users/svanginhoven/Documents/GPI Projects/gpi_data_cleaning/data/goupil_stock_no_concordance.csv")
+goupil <- read_csv("/Users/svanginhoven/Documents/GPI Projects/gpi_data_cleaning/data/goupil.csv")
+# correct colum names -- look for code for that
+goupil_colnames <- colnames(goupil)
+goupil_colnames <- goupil_colnames %>%
+  str_trim() %>%
+  str_replace_all("[[:punct:] ]+$", "") %>%
+  str_replace_all("[[:punct:] ]+", "_") %>%
+  tolower()
+colnames(goupil) <- goupil_colnames
+goupil <- goupil %>%
+  mutate(star_record_no = as.character(star_record_no))
+
+## IDENTIFY RELATED OBJECTS AND CONNECT THEM VIA COMPONENTS:
 # Editors have compiled links between stocknumbers that actually represent the
 # same object. We compose a graph of these relationships, identify connected
 # components that represnent all the stock numbers related to a single object,
 # and then produce a lookup table pairing each stock number with a UID for its
 # group
-produce_goupil_stocknumber_concordance <- function(raw_goupil_stocknumber_concordance, goupil_untrustworthy_numbers) {
+produce_goupil_stocknumber_concordance <- function(raw_goupil_stocknumber_concordance) {
   # Produce a 'long' table from the 'wide' version entered by editors
   goupil_stocknumber_concordance <- raw_goupil_stocknumber_concordance %>%
     select(contains("sn")) %>%
     mutate(prime_stock_number = sn1) %>%
     gather(number_index, stock_number, contains("sn"), na.rm = TRUE) %>%
     select(source = prime_stock_number, target = stock_number) %>%
-    filter(!(source %in% goupil_untrustworthy_numbers$goupil_number) & !(target %in% goupil_untrustworthy_numbers$goupil_number)) %>%
+    #filter(!(source %in% goupil_untrustworthy_numbers$goupil_number) & !(target %in% goupil_untrustworthy_numbers$goupil_number)) %>%
     na.omit()
   
   # Create a graph from this edgelist
@@ -23,15 +59,19 @@ produce_goupil_stocknumber_concordance <- function(raw_goupil_stocknumber_concor
   sn_components <- igraph::components(sn_graph)
   igraph::V(sn_graph)$component <- sn_components$membership
   goupil_stocknumber_concordance <- igraph::as_data_frame(sn_graph, what = "vertices") %>%
-    rename(goupil_number = name)
+    rename(star_record_no = name)
   
   goupil_stocknumber_concordance
 }
 
+goupil_concordance <- produce_goupil_stocknumber_concordance(raw_goupil_stocknumber_concordance)
+
+## PRODUCE UNIQUE IDS
+# TO DO: use uuid package
 # Produce unique ids for goupil objects based on their stock numbers
-identify_goupil_objects <- function(df, goupil_stocknumber_concordance) {
+identify_goupil_objects <- function(df, goupil_concordance) {
   df %>%
-    left_join(goupil_stocknumber_concordance, by = c("goupil_number")) %>%
+    left_join(goupil_concordance, by = "star_record_no") %>%
     # Because some of the goupil stock numbers changed or were re-used, we
     # will consult against a stock number concordance that we can use to create
     # a "functional" stock number - an identifier that connects objects even
@@ -41,17 +81,21 @@ identify_goupil_objects <- function(df, goupil_stocknumber_concordance) {
     mutate(
       prepped_sn = case_when(
         # When there is no number, generate a unique ID
-        is.na(goupil_number) ~ paste("gennum", as.character(seq_along(star_record_no)), sep = "-"),
+        is.na(star_record_no) ~ paste("gennum", as.character(seq_along(nrow(df))), sep = "-"),
         # When there is a number that has a prime # replacement from the
         # concordance, use that prime #
         !is.na(component) ~ paste("componentnum", component, sep = "-"),
         # When the original number has no recorded changes, group based on that
         # original number
-        TRUE ~ paste("orignnum", goupil_number, sep = "-"))) %>%
+        TRUE ~ paste("orignnum", star_record_no, sep = "-"))) %>%
     mutate(object_id = paste("k", "object", group_indices(., prepped_sn), sep = "-")) %>%
     select(-component, -prepped_sn)
 }
 
+goupil_objects <- identify_goupil_objects(goupil, goupil_concordance)
+
+
+## ORDERING EVENTS
 # For a given object_id, attempt to discern an event order, which can be useful
 # for discerning timespand boundaries as well as figuring out when an object
 # first entered, and then finally left, Goupil's collection.
